d91636ea6b32788ee2906350abff70caeae2c1ce
odilo <odilorugamba@gmail.com>
2021-04-07 12:37:55 +0200

    get to post

--- apps/question-bank/router.js
+++ apps/question-bank/router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/', QuestionBank.create);
 router.put('/:questionBankId', QuestionBank.update);
 router.get('/:unitId/unit-question-bank', QuestionBank.getUnitQuestionBank);
-router.get('/units-list-question-bank', QuestionBank.getUnitsListQuestionBank);
+router.post('/units-list-question-bank', QuestionBank.getUnitsListQuestionBank);
 router.get('/:subjectId/subject-question-bank', QuestionBank.getSubjectQuestionBank);
 router.get('/:questionBankId', QuestionBank.getOneQuestionBank);
 router.delete('/:questionBankId', QuestionBank.delete);
